a9d2722d2f797a5754b62345a75147c7c53c28bf
joewish <72031491+joewish@users.noreply.github.com>
2024-08-31 16:21:32 +0530

    fix-6

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import router from './src/doctors/routes/doctor.routes.js';
 import router2 from './src/patients/routes/patients.routes.js';
+import swagger from 'swagger-ui-express';
+import apiDocs from './swagger.json' assert {type: 'json'};
+import expressEjsLayouts from "express-ejs-layouts";
 // Load environment variables from .env file
 dotenv.config({ path: path.resolve("config","uat.env") });
 
@@ -17,6 +20,16 @@ app.use(cookieParser())
 app.use("/doctors", router);
 app.use("", router2);
 
+app.use("/api-docs",
+  swagger.serve,
+  swagger.setup(apiDocs))
+app.get('/', (req, res) => {
+  res.redirect('/api-docs')
+  // res.send(`
+  //   <h1>Welcome to the Hospital API</h1>
+  //   <p>Click <a href="http://localhost:3000/api-docs/">here</a> to test the API with Swagger</p>
+  // `);
+})
 
 // Start the server
 app.listen(process.env.PORT || 8000, async (error) => {
